46365cb37e0d852ea5504f0add9f4e32bdc75090
Saneksking <saneksking@gmail.com>
2023-12-25 23:17:32 +0700

    Добавили базовый шаблон и шаблон главной страницы

--- main/views.py
+++ main/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    context = {
+
+    }
+    return render(request, 'main/index.html', context)
