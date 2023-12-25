49923041b4d0455b77b24b0d9aae80f6f9d3c82e
Saneksking <saneksking@gmail.com>
2023-12-26 00:53:31 +0700

    Добавил пагинатор для post_list.html.

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from posts.models import Post
@@ -5,8 +6,11 @@ from posts.models import Post
 
 def posts(request):
     post_list = Post.objects.filter(status=True)
+    paginator = Paginator(post_list, 10)
+    page = request.GET.get('page')
+    objects = paginator.get_page(page)
     context = {
-        'post_list': post_list,
+        'objects': objects,
     }
     return render(request, 'posts/post_list.html', context)
 
